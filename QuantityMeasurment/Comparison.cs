32481e7620bf70d1fc7311b08034dd3463ba102f
Saksham27 <geekysaksham27@gmail.com>
2020-05-05 04:52:23 +0530

    Added: method to compare given inch with feet

---
+++ QuantityMeasurment/Comparison.cs
@@ -0,0 +1,36 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="Comparison.cs" company="BridgeLabz Solutions LLP">
+//     Copyright (c) Company. All rights reserved.
+// </copyright>
+// <author> Saksham Singh </author>
+//-----------------------------------------------------------------------
+namespace QuantityMeasurment
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    /// <summary>
+    /// comparison class containing different comparisons
+    /// </summary>
+    public class Comparison
+    {
+        /// <summary>
+        /// method to compare inches with feet
+        /// </summary>
+        /// <param name="inch"> inch input given by user </param>
+        /// <param name="feet"> feet input given by user </param>
+        /// <returns> true is both equal else false </returns>
+        public bool CompareInchWithFeet(double inch, double feet)
+        {
+            // cheking if inch is equal to feet, return true
+            // if not : return false
+            if (inch / 12 == feet)
+            {
+                return true;
+            }
+
+            return false;
+        } //// end : public bool CompareInchWithFeet(double inch, double feet)
+    } //// end : class Comparison
+} //// end : namespace QuantityMeasurment
---
+++ QuantityMeasurment/Measurment.cs
@@ -0,0 +1,25 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="Measurement.cs" company="BridgeLabz Solutions LLP">
+//     Copyright (c) Company. All rights reserved.
+// </copyright>
+// <author> Saksham Singh </author>
+//-----------------------------------------------------------------------
+namespace QuantityMeasurment
+{
+    using System;
+
+    /// <summary>
+    /// class containing main method
+    /// </summary>
+    public class Measurement
+    {
+        /// <summary>
+        /// main method
+        /// </summary>
+        /// <param name="args"> run time arguments </param>
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Quantity measurment");
+        } //// end : public static void Main(string[] args)
+    } //// end : class Program
+} //// end : namespace QuantityMeasurment
