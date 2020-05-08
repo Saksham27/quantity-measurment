d8a59133253eb771c329b64f2d2f1c07dd97ad9e
Saksham27 <geekysaksham27@gmail.com>
2020-05-08 13:20:37 +0530

    Added : Code for Test Case 1.24

---
+++ QuantityMeasurment/Calculate.cs
@@ -0,0 +1,36 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="Calculate.cs" company="BridgeLabz Solutions LLP">
+//     Copyright (c) Company. All rights reserved.
+// </copyright>
+// <author> Saksham Singh </author>
+//-----------------------------------------------------------------------
+namespace QuantityMeasurement
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    /// <summary>
+    /// class containing diffrent calculations
+    /// </summary>
+    public class Calculate
+    {
+        /// <summary>
+        /// method to add two quantiities
+        /// </summary>
+        /// <param name="quantityOne"> first value </param>
+        /// <param name="quantityTwo"> second value </param>
+        /// <returns> sum in inches </returns>
+        public static double AddToInch(Measure quantityOne, Measure quantityTwo)
+        {
+            if (quantityOne.Unit != 0 && quantityTwo.Unit != 0)
+            {
+                return (int)quantityOne.Unit/(int)Units.inch * quantityOne.Length + (int)quantityTwo.Unit/(int)Units.inch * quantityTwo.Length;
+            }
+            else
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+        } /// end  : public static double AddToInch(Measure quantityOne, Measure quantityTwo)
+    } //// end  : class Calculate
+} //// end : namespace QuantityMeasurment
